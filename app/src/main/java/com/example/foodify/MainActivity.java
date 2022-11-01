a9c488b715dd3d5a14e05fe08ff5b418737f320b
anthony <anthony.plasencia29511@gmail.com>
2022-11-01 16:51:54 -0700

    Changed register screen to a login Screen no functionality yet

--- app/src/main/java/com/example/foodify/MainActivity.java
+++ app/src/main/java/com/example/foodify/MainActivity.java
@@ -10,23 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private static String TAG = "MainActivity";
+import com.google.android.material.button.MaterialButton;
 
-    private EditText inputName , inputEmail, inputPass, inputPassRepeat ;
-    private Button btnRegister;
-    private TextView txtWarnName,txtWarmEmail,txtWarmPass,txtWarnPassRepeat;
-    private ConstraintLayout parent;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initViews();
+        TextView username = (TextView)findViewById(R.id.username);
+        TextView password = (TextView)findViewById(R.id.password);
+
+        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
     }
 
-    private void initViews(){
-        Log.d(TAG,"initCiews: Started");
-    }
+
+
+
 }
