ff054cbae2356663078732ee83a5e66c10b24715
anthony <anthony.plasencia29511@gmail.com>
2022-11-06 15:11:05 -0800

    Added sign in with google not sure if its only my google account

--- app/src/main/java/com/example/foodify/MainActivity.java
+++ app/src/main/java/com/example/foodify/MainActivity.java
@@ -1,31 +1,83 @@
 package com.example.foodify;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.media.ImageWriter;
 import android.os.Bundle;
 import android.provider.SyncStateContract;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView googleBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username = (TextView)findViewById(R.id.username);
-        TextView password = (TextView)findViewById(R.id.password);
+        googleBtn = findViewById(R.id.google_btn);
 
-        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        googleBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+    });
+
+
+
+
+}
+
+    private void signIn() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
+            try {
+                task.getResult(ApiException.class);
+                navigateToHomeScreen();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_LONG).show();
+            }
+        }
 
+    }
 
+    void navigateToHomeScreen() {
+        finish();
+        Intent intent = new Intent(MainActivity.this,HomeScreen.class);
+        startActivity(intent);
+    }
 }
