1df41eaa837731dee8606066b3ce1905f3a396c9
erinjmaldonado <39362404+erinjmaldonado@users.noreply.github.com>
2022-11-21 19:48:47 -0800

    Update Register.java

--- app/src/main/java/com/example/foodify/Register.java
+++ app/src/main/java/com/example/foodify/Register.java
@@ -1,91 +1,103 @@
 package com.example.foodify;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.parse.ParseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
     private TextView back;
     private Button signUp;
-    private TextInputEditText username;
+
     private TextInputEditText password;
     private TextInputEditText passwordagain;
     private TextInputEditText email;
     private ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         progressDialog = new ProgressDialog(Register.this);
 
         back = findViewById(R.id.alreadyHaveAccount);
         signUp = findViewById(R.id.register);
-        username = findViewById(R.id.inputName);
         email = findViewById(R.id.inputEmail);
         password = findViewById(R.id.inputPassword);
         passwordagain = findViewById(R.id.inputConfirmPassword);
 
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
-        signUp.setOnClickListener(v -> {
-            if (password.getText().toString().equals(passwordagain.getText().toString()) && !TextUtils.isEmpty(username.getText().toString()))
-                signUp(username.getText().toString(), password.getText().toString(), email.getText().toString());
-            else
-                Toast.makeText(this, "Make sure that the values you entered are correct.", Toast.LENGTH_SHORT).show();
+        back.setOnClickListener(v -> {
+            startActivity(new Intent(Register.this, MainActivity.class));
         });
 
-        back.setOnClickListener(v -> finish());
+        signUp.setOnClickListener(v ->{
+                auth();
+        });
 
     }
 
-    private void signUp(String username, String password, String email) {
-        progressDialog.show();
-        ParseUser user = new ParseUser();
-        // Set the user's username and password, which can be obtained by a forms
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setEmail(email);
-        user.signUpInBackground(e -> {
-            progressDialog.dismiss();
-            if (e == null) {
-                showAlert("Successful Sign Up ! You logged in...\n", "Welcome " + username + " !");
-            } else {
-                ParseUser.logOut();
-                Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(Register.this, ProfileActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
+    private void auth(){
+        String emailStr = email.getText().toString();
+        String passwordStr = password.getText().toString();
+        String passwordagainStr = passwordagain.getText().toString();
 
-    private void showAlert(String title, String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(Register.this)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                        // don't forget to change the line below with the names of your Activities
-                        Intent intent = new Intent(Register.this, LogoutActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
+        if(!TextUtils.isEmpty(emailStr) && !Patterns.EMAIL_ADDRESS.matcher(emailStr).matches()){
+            email.setError("Enter Correct email");
+        } else if(passwordStr.isEmpty() || password.length() < 6){
+            passwordagain.setError("Incorrect Password");
+        } else if(!passwordStr.equals(passwordagainStr)){
+            passwordagain.setError("Passwords do not match");
+        } else {
+            progressDialog.setMessage("Registering...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(emailStr, passwordStr).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(Register.this, "Registration Successful", Toast.LENGTH_SHORT);
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(Register.this, ""+task.getException(), Toast.LENGTH_SHORT);
                     }
-                });
-        AlertDialog ok = builder.create();
-        ok.show();
+                }
+            });
+        }
     }
 
 }
