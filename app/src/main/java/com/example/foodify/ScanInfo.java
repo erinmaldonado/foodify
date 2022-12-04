587d35c2befb3bdb29bfcd5998dd109db778e05a
Erin Maldonado <erinjmaldonado@gmail.com>
2022-12-03 17:41:27 -0800

    scanned item is opened in new fragment

--- app/src/main/java/com/example/foodify/ScanInfo.java
+++ app/src/main/java/com/example/foodify/ScanInfo.java
@@ -1,19 +1,119 @@
 package com.example.foodify;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.BuildConfig;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.gson.Gson;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class ScanInfo extends Fragment {
+    JsonResponse jsonResponse;
+    String upc;
+
+    Gson gson = new Gson();
+    int minteger = 0;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan_info, container, false);
+        scan();
         return view;
+    }
+
+    void scan(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Scan a barcode");
+        options.setCameraId(0);
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(BarcodeScannerActivity.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            upc = result.getContents();
+            sendUserToBarCodeInfo(upc);
+        }
+    });
+
+
+
+    private void sendUserToBarCodeInfo(String upc){
+        TextView textViewResult = getView().findViewById(R.id.textViewResult);
+        String Key;
+        Key = "55d01a0c91msh1a5d4e55f6cf63cp174b8bjsn419908648873"; // FIX
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/upc/"+upc;
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("X-RapidAPI-Key", Key)
+                .addHeader("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String myResponse = response.body().string();
+
+                    jsonResponse =gson.fromJson(myResponse, JsonResponse.class);
+
+                    getActivity().runOnUiThread(() ->{
+                        textViewResult.setText(jsonResponse.getTitle() + " id: " + jsonResponse.getId());
+                    });
+                }
+            }
+
+        });
+    }
+
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) getView().findViewById(
+                R.id.total);
+        displayInteger.setText("" + number);
     }
 }
